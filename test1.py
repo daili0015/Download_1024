f44ec9a9a968490346da3827a44fb71e8529f567
daili0015 <daili0015@gmail.com>
2016-12-02 14:17:21 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -14,7 +14,8 @@ headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (
             'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             'Accept-Encoding':'gzip',
            }#初始使用的header
-URL_1024='你猜啊，就不告诉你'#网站地址中‘日本骑兵’系列，别的系列我没有测试，不保证正确
+URL_1024='github已自动隐藏该信息'#网站地址中‘日本骑兵’系列，别的系列我没有测试，不保证正确 #刚测试了，‘亚洲无码’也可以，估计所有系列的网页HTML格式是一样的
+                                    
 
 start_html = requests.get(URL_1024,  headers=headers)
 start_html.encoding='utf-8'
